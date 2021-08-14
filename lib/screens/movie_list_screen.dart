068de17f6962142af30771e3af9f2cb74455eb81
harshthakur066 <harshthakur066@gmail.com>
2021-08-14 21:00:52 +0530

    movie list screen

--- lib/screens/movie_list_screen.dart
+++ lib/screens/movie_list_screen.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:movie_list_app/providers/movies.dart';
+import 'package:provider/provider.dart';
 
-class MovieListScreen extends StatefulWidget {
-  MovieListScreen({Key? key}) : super(key: key);
+import 'package:movie_list_app/screens/movie_add_screen.dart';
 
-  @override
-  _MovieListScreenState createState() => _MovieListScreenState();
-}
+class MovieListScreen extends StatelessWidget {
+  const MovieListScreen({Key? key}) : super(key: key);
 
-class _MovieListScreenState extends State<MovieListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // Navigator.of(context).pushNamed(MovieAddScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<Movies>(context, listen: false).fetchData(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<Movies>(
+                child: null,
+                builder: (ctx, movies, _) => movies.items.length <= 0
+                    ? Center(
+                        child:
+                            const Text('Got no movies yet, start adding some!'),
+                      )
+                    : ListView.builder(
+                        itemCount: movies.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              movies.items[i].image,
+                            ),
+                          ),
+                          title: Text(movies.items[i].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
+                        ),
+                      ),
+              ),
       ),
     );
   }
